29b7f8e3b92e4ea7d343056bbdc50c12cd311438
n00b St4r <realgoldcraft@gmail.com>
2024-07-02 13:28:48 +0700

    change bun to pnpm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,9 @@ export default {
   driver: 'turso',
   dbCredentials: {
     // url: 'file:test.db'
+    //@ts-ignore
     url: process.env.DATABASE_URL ?? '',
+    //@ts-ignore
     authToken: process.env.DATABASE_AUTH_TOKEN ?? ''
   }
 } satisfies Config;
