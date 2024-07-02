d458d6d2da5e1498e695dfc76f924f5d6583be1f
n00b St4r <realgoldcraft@gmail.com>
2024-07-02 18:32:37 +0700

    add dashboard

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,3 +1,53 @@
+import { db } from '$lib/server';
+import { piutangTable, utangTable } from '$lib/server/schema/keuangan';
+import { count, sql, sum } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
+import { fakturPembelianTable, supplierTable } from '$lib/server/schema/pembelian';
+import { barangTable, fakturPenjualanTable } from '$lib/server/schema/penjualan';
 
-export const load: PageServerLoad = async () => {};
+export const load: PageServerLoad = async () => {
+  const utang = await db
+    .select({
+      jumlah: sql<number>`coalesce(sum(${utangTable.nilai}), 0)`
+    })
+    .from(utangTable);
+
+  const fakturPembelian = await db
+    .select({
+      jumlah: sql<number>`coalesce(count(${fakturPembelianTable.id}), 0)`
+    })
+    .from(fakturPembelianTable);
+
+  const piutang = await db
+    .select({
+      jumlah: sql<number>`coalesce(sum(${piutangTable.nilai}), 0)`
+    })
+    .from(piutangTable);
+
+  const fakturPenjualan = await db
+    .select({
+      jumlah: sql<number>`coalesce(count(${fakturPenjualanTable.id}), 0)`
+    })
+    .from(fakturPenjualanTable);
+
+  const barang = await db
+    .select({
+      jumlah: sql<number>`coalesce(count(${barangTable.id}), 0)`
+    })
+    .from(barangTable);
+
+  const supplier = await db
+    .select({
+      jumlah: count(supplierTable.id)
+    })
+    .from(supplierTable);
+
+  return {
+    utang,
+    fakturPembelian,
+    fakturPenjualan,
+    piutang,
+    barang,
+    supplier
+  };
+};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,41 @@
-<div>ini dashboard</div>
+<script lang="ts">
+  import { Boxes, Store } from 'lucide-svelte';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="grid grid-cols-4 gap-4">
+  <div class="flex flex-col gap-4 rounded-lg border-2 p-4">
+    <h1 class="text-lg font-semibold">Hutang</h1>
+    <div class="text-2xl font-bold">
+      Rp {data.utang[0].jumlah}
+    </div>
+    <p>Jumlah Faktur Pembelian {data.fakturPembelian[0].jumlah}</p>
+  </div>
+  <div class="flex flex-col gap-4 rounded-lg border-2 p-4">
+    <h1 class="text-lg font-semibold">Piutang</h1>
+    <div class="text-2xl font-bold">
+      Rp {data.piutang[0].jumlah}
+    </div>
+    <p>Jumlah Faktur Penjualan {data.fakturPenjualan[0].jumlah}</p>
+  </div>
+  <div class="flex flex-row items-center gap-4 rounded-lg border-2 p-4">
+    <Boxes size={80} strokeWidth={0.75} class="h-fit w-auto rounded-full bg-blue-200 p-2" />
+    <div class="flex w-full flex-col items-end justify-center gap-2 text-right">
+      <p class="text-2xl font-bold">
+        {data.barang[0].jumlah}
+      </p>
+      <p class="text-lg">Barang</p>
+    </div>
+  </div>
+  <div class="flex flex-row items-center gap-4 rounded-lg border-2 p-4">
+    <Store size={80} strokeWidth={0.75} />
+    <div class="flex w-full flex-col items-end justify-center gap-2 text-right">
+      <p class="text-2xl font-bold">
+        {data.supplier[0].jumlah}
+      </p>
+      <p class="text-lg">Supplier</p>
+    </div>
+  </div>
+</div>
